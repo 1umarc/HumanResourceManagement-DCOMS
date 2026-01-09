55346ff61465db4bc324b5c035c05395b6436680
1umarc <luven04@gmail.com>
2026-01-09 15:45:56 +0800

    rmi testing

---
+++ HelloClient.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package rmi;
+
+/**
+ *
+ * @author luven
+ */
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry; 
+import java.rmi.Naming; // to skip step 1
+
+public class HelloClient {
+    public static void main(String[] args) {
+        try {
+            // Step 1: Get reference to the RMI registry on the server
+            // If client runs on the same machine as server, use "localhost" --> 1 tier
+            Registry registry = LocateRegistry.getRegistry("localhost", 1099); // variable = "localhost" or IP or hostname
+
+            // Step 2: Look up the remote object by the same name used in server
+            HelloService helloService = (HelloService) registry.lookup("HelloService"); // interface interface = (interface)--> () typecasting - interface treated as object
+                        // helloService --> STUB, implementing the interface
+            // Step 3: Invoke remote method as if local object
+            String response = helloService.sayHello("Umapathy"); // networking...
+            
+            // Step 4: Use/print the result
+            System.out.println("Response from server: " + response);
+            
+//            AddInterface obj = (AddInterface) Naming.lookup("rmi://localhost/AddService"); --> RMI registry running in a separate process, common production practice
+//            int result = obj.add(10, 20);
+//            System.out.println("Result of addition: " + result);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
